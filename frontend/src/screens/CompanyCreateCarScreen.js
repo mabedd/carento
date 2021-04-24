61a780409d02893413485e0793253485f5cda32e
mohammed-ab99 <moa.abed99@gmail.com>
2021-04-25 02:01:41 +0300

    Update CompanyCreateCarScreen.js

--- frontend/src/screens/CompanyCreateCarScreen.js
+++ frontend/src/screens/CompanyCreateCarScreen.js
@@ -114,7 +114,7 @@ const CompanyCreateCarScreen = ({ location, history, match }) => {
                 },
             }
 
-            const { data } = await axios.post('/api/upload', formData, config)
+            const { data } = await axios.post('http://localhost:5000/api/upload', formData, config)
 
             setImage(data)
             setUploading(false)
